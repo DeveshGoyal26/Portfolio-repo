a2f7f3c309aca0ded2609f67e3c4cbeea4efde25
Devesh Goyal <Devesh26goyal@gmail.com>
2022-05-28 17:57:12 +0530

    Added all the necessary files

---
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./Navbar.scss";
+// import { images } from "../../constants";
+import { motion } from "framer-motion";
+import { HiMenuAlt4, Hix } from "react-icons/hi";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        {/* <img src={images.logo} alt="logo" /> */}
+        <span>DG</span>
+      </div>
+      <ul className="app__navbar-links">
+        {["Home", "About", "Contact", "Work", "Skills"].map((el) => {
+          return (
+            <li className="app__flex p-text" key={`link-${el}`}>
+              <div></div>
+              <a href={`#${el}`}>{el}</a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && <motion.div whileInView={{ x: [300, 0] }} ta></motion.div>}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
