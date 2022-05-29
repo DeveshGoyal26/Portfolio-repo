8bc301c5c88de397a593e7fc89069ac35f61c252
Devesh Goyal <Devesh26goyal@gmail.com>
2022-05-30 01:10:55 +0530

    Update client.js

--- front-end/src/client.js
+++ front-end/src/client.js
@@ -7,6 +7,7 @@ export const client = sanityClient({
   apiVersion: "2022-03-30",
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true,
 });
 
 const builder = imageUrlBuilder(client);
